eacda722ed025f4a8d63cffc76cac62f9111bc7e
Alex Keselman <alex@vir-tec.net>
2025-01-06 10:54:15 +0200

    060125

--- pages/coin/[id].js
+++ pages/coin/[id].js
@@ -2,17 +2,27 @@ import { useRouter } from "next/router"
 import { MainLayout } from "../../components/layout"
 import Link from "next/link"
 
-export default function Post() {
+export default function Coin({data}) {
     const router = useRouter()
     //console.log(router)
     return (
         <MainLayout title="Coins">
             <div className="max-w-[1000px] my-4 mx-auto flex justify-between">
                 <div className="text-3xl text-blue-600 font-bold">Coin {router.query.id}</div>
+                
                 <div className="pt-3">
                     <Link href="/coins" className="text-blue-800 font-bold hover:underline">Back to All Coins</Link>
                 </div>
             </div>
+            <pre>{JSON.stringify(data,null,4)}</pre>
         </MainLayout>
     )
-}
+} 
+
+export async function getServerSideProps(req) {
+    const resp = await fetch(`https://api.mtw-testnet.com/assets/${req.query.id}`)
+    const data = await resp.json()
+    delete data["summary"]
+    // Pass data to the page via props
+    return { props: { data } }
+  }
---
+++ pages/coins1.js
@@ -0,0 +1,33 @@
+import { useEffect, useState } from "react"
+import { MainLayout } from "../components/layout"
+import { useRouter } from "next/router"
+
+//export default function Coins({data}) {
+export default function Coins() {
+    const router = useRouter()
+ const [data,setData] = useState(null)
+
+    useEffect(() => {
+        fetch('https://api.mtw-testnet.com/assets/symbols')
+        .then(res => res.json())
+        .then(d => setData(d))
+    }, []) 
+
+    return (
+        <MainLayout title="Coins">
+            <div className="max-w-[1000px] my-4 mx-auto text-3xl text-blue-600 font-bold">Coins</div>
+            {data && data.length > 0 ? (
+                <div>
+                    {data.map((coin) => (
+                        <div className="py-1 px-6 m-1 bg-green-200 hover:bg-green-300 cursor-pointer rounded float-left" 
+                        onClick={() => router.push(`/coin/${coin}`)} key={coin}>{coin}</div>
+                    ))}
+                    <div className="clear-both"></div>
+                </div>
+            ) : (
+                <div>Loading...</div>
+            )}
+        </MainLayout>
+    )
+}
+
